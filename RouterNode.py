1d0512309d438967275852d7b8bae2afcce067f2
Tella <tellarinipietro@gmail.com>
2023-03-10 10:24:53 +0100

    Update RouterNode.py

--- RouterNode.py
+++ RouterNode.py
@@ -29,7 +29,7 @@ class RouterNode():
         # Initialize class variables
         self.poisonReverse = self.sim.POISONREVERSE
         self.minCosts = deepcopy(costs)
-        self.immidiateNeighbours = self.minCosts
+        self.immidiateNeighbours = deepcopy(costs)
         self.neighbourCosts = [[self.sim.INFINITY]*self.sim.NUM_NODES] * self.sim.NUM_NODES
         self.neighbourCosts[ID] = costs        
         self.nextHop = [-1] * self.sim.NUM_NODES
@@ -81,19 +81,29 @@ class RouterNode():
 
     # --------------------------------------------------
     def updateLinkCost(self, dest, newcost):
+        newcost = 60
         self.myGUI.println("\nUpdating link cost from " + str(self.myID) + " to " + str(dest) + " of " + str(newcost) + "\n")
         self.immidiateNeighbours[dest] = newcost
-        self.minCosts[dest] = newcost
+
+        if self.nextHop[dest] > newcost:
+            self.nextHop[dest] = newcost
+            self.minCosts[dest] = newcost
+        else:
+            self.minCosts[dest] = self.nextHop[dest]
+
         self.neighbourCosts[self.myID][dest] = newcost
 
+        self.minCosts[dest] = newcost
+        self.sendUpdate()
+
         #self.minCosts[dest] = min(newcost, )
-        for i in range (self.sim.NUM_NODES):
+        """ for i in range (self.sim.NUM_NODES):
             if (i!=self.myID):
-                newCost = self.neighbourCosts[self.myID][dest] + self.neighbourCosts[].mincost[i]
+                newCost = self.neighbourCosts[self.myID][dest] + self.neighbourCosts[dest]
                 if(self.minCosts[i] > newCost):
                     self.minCosts[i] = newCost
                     self.neighbourCosts[self.myID][i] = self.minCosts[i]
                     self.nextHop[i] = pkt.sourceid
                     self.sendUpdate()
-
+ """
         #self.recvUpdate(RouterPacket.RouterPacket(dest, self.myID, self.minCosts))
