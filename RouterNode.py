1055b3e2b85121cb0854d645fe3cc18019c0df66
nsar69 <nsarnin@gmail.com>
2023-03-12 17:39:39 +0100

    working version

--- RouterNode.py
+++ RouterNode.py
@@ -10,12 +10,11 @@ class RouterNode():
 
     # Variable declaration
     neighbourCosts = None
+    routingTable=None
     minCosts = None
     nextHop = None
     poisonReverse = False
-    immidiateNeighbours = None
-    prevNeighbourCosts = None
-    
+
 
     # Access simulator variables with:
     # self.sim.POISONREVERSE, self.sim.NUM_NODES, etc.
@@ -28,58 +27,70 @@ class RouterNode():
 
         # Initialize class variables
         self.poisonReverse = self.sim.POISONREVERSE
-        self.minCosts = deepcopy(costs)
-        self.immidiateNeighbours = deepcopy(costs)
 
-        self.neighbourCosts = [[self.sim.INFINITY]*self.sim.NUM_NODES] * self.sim.NUM_NODES
-        self.neighbourCosts[ID] = costs        
+        self.neighbourCosts=deepcopy(costs)
+        self.minCosts = deepcopy((costs))
+        self.routingTable = [[self.sim.INFINITY]*self.sim.NUM_NODES] * self.sim.NUM_NODES
+        self.routingTable[self.myID]=deepcopy(self.neighbourCosts)
         self.nextHop = [-1] * self.sim.NUM_NODES
+
         for i in range(self.sim.NUM_NODES):
                 if (costs[i] < self.sim.INFINITY):
                     self.nextHop[i] = i
-
-
         self.myGUI.println("My initial routing table is :")
-        self.myGUI.println(str(self.neighbourCosts) +"\n")
+        self.myGUI.println(str(self.routingTable) +"\n")
 
         self.prepare_send()
 
     
     # --------------------------------------------------
     def recvUpdate(self, pkt):
-        self.myGUI.println("Got an update from " + str(pkt.sourceid) +
-                ", its minimum costs is : " + str(pkt.mincost))
-        self.neighbourCosts[pkt.sourceid] = pkt.mincost
-        for i in range (self.sim.NUM_NODES):
-
-            if (i!=self.myID):
-                newCost = self.neighbourCosts[self.myID][pkt.sourceid] + pkt.mincost[i]
-                if (self.minCosts[i] > newCost):
-                    self.minCosts[i] = newCost
-                    self.neighbourCosts[self.myID][i] = self.minCosts[i]
-                    self.nextHop[i] = pkt.sourceid
-                    self.prepare_send()
-    def prepare_send(self):
-        self.myGUI.println("My new min cost is: "+ str(self.minCosts))
+        self.myGUI.println("I'm "+ str(self.myID) + ", Got an update from " + str(pkt.sourceid) +
+                           ", its news minimum costs are : " + str(pkt.mincost))
+        print("I'm "+ str(self.myID) + ", Got an update from " + str(pkt.sourceid) +
+        ", its news minimum costs are : " + str(pkt.mincost))
+        self.routingTable[pkt.sourceid] = pkt.mincost
+        print("My currently min costs are"+ str(self.minCosts))
+        previousMinCosts = deepcopy(self.minCosts)
+        print("My currently min costs are" + str(previousMinCosts))
         for i in range(self.sim.NUM_NODES):
-            if (self.immidiateNeighbours[i] < self.sim.INFINITY and i != self.myID):
-                # Poisoned Reversed
-                falseMincost = deepcopy(self.minCosts)
-                if self.sim.POISONREVERSE:
-                    falseMincost = deepcopy(self.minCosts)
-                    if self.nextHop[i] != i:
-                            falseMincost[i] = self.sim.INFINITY
+            if i != self.myID:
+                self.myGUI.println("Compute min cost to go to " + str(i) + " from " + str(self.myID))
+                possibleCost = self.neighbourCosts[i]
+                print(" Cost to go directly to "+ str(i) + " is "+ str(possibleCost))
+                for j in range(self.sim.NUM_NODES):
+                    if (j != i and j != self.myID):
+                        possibleCost1 = self.neighbourCosts[j] + self.routingTable[j][i]
+                        print("possible cost by " + str(j) + " to go to " + str(i)  +" is " + str(possibleCost1))
+                        if possibleCost1 < possibleCost:
+                            possibleCost = possibleCost1
+                            self.nextHop[i]=j
+                self.minCosts[i] = possibleCost
+                print("Finally my min cost to go to "+ str(i) +" is " +str(self.minCosts[i]))
 
-                    pkt = RouterPacket.RouterPacket(self.myID, i, falseMincost)
-                    self.myGUI.println("At time " + str(self.sim.getClocktime()) +
-                        " I'm sending my minimum cost " + str(falseMincost) + "to " + str(pkt.destid))
-                    self.sendUpdate(pkt)
-                else:#end Poisoned Reverse
-                    pkt = RouterPacket.RouterPacket(self.myID, i, deepcopy(self.minCosts))
-                    self.myGUI.println("At time " + str(self.sim.getClocktime()) +
-                                       " I'm sending my minimum cost " + str(falseMincost) + "to " + str(pkt.destid))
-                    self.sendUpdate(pkt)
+        print("Finally my min cost after computation is " + str(self.minCosts))
+        print("My previous one was: "+ str(previousMinCosts))
+        if self.minCosts!=previousMinCosts:
+            print(" chaneg so I send new one")
+            self.prepare_send()
+        else:
+            print("No change, I do nothing")
 
+
+
+    #-------------------------------
+    def prepare_send(self):
+        self.myGUI.println("My new min cost is: " + str(self.minCosts))
+        self.routingTable[self.myID]=self.minCosts
+        for i in range(self.sim.NUM_NODES):
+            if (self.neighbourCosts[i] < self.sim.INFINITY and i != self.myID):
+
+                pkt = RouterPacket.RouterPacket(self.myID, i, deepcopy(self.minCosts))
+                self.myGUI.println("At time " + str(self.sim.getClocktime()) +
+                                       " I'm sending my minimum cost " + str(self.minCosts) + "to " + str(pkt.destid))
+                self.sendUpdate(pkt)
+
+        self.myGUI.println("\n")
     # --------------------------------------------------
     def sendUpdate(self,pkt):
         self.sim.toLayer2(pkt)
@@ -87,29 +98,39 @@ class RouterNode():
 
     # --------------------------------------------------
     def printDistanceTable(self):
-        if(self.neighbourCosts != self.prevNeighbourCosts):
-            self.myGUI.println("Current routing table for " + str(self.myID) +
-                            "  at time " + str(self.sim.getClocktime()))
-            self.myGUI.println(str(self.neighbourCosts) + "\n")
-            self.prevNeighbourCosts = deepcopy(self.neighbourCosts)
-            # We print the route for each
-            for i in range(self.sim.NUM_NODES):
-                if i!=self.myID:
-                    self.myGUI.println("Next hop to go to "+ str(i)+
-                                   " is "+ str(self.nextHop[i]))
+
+        self.myGUI.println("Current routing table for " + str(self.myID) +
+                        "  at time " + str(self.sim.getClocktime()))
+        self.myGUI.println(str(self.routingTable) + "\n")
+        self.myGUI.println("My neighbour costs are : "+ str(self.neighbourCosts) + "\n")
+        # We print the route for each
+        for i in range(self.sim.NUM_NODES):
+            if i!=self.myID:
+                self.myGUI.println("Next hop to go to "+ str(i)+
+                        " is "+ str(self.nextHop[i]))
         self.myGUI.println("\n")
     # --------------------------------------------------
     def updateLinkCost(self, dest, newcost):
-        self.myGUI.println("\nUpdating link cost between " + str(self.myID) + " to " + str(dest) + " of " + str(newcost) + "\n")
-        self.immidiateNeighbours[dest] = newcost
+        self.myGUI.println("updated link cost to " + str(dest) + "(" + str(newcost) + ")")
 
-        #New computation
         for i in range(self.sim.NUM_NODES):
-            if (i != self.myID):
-                updateCost= newcost+ self.neighbourCosts[dest][i]
-                if (self.minCosts[i] > updateCost):
-                    self.minCosts[i] = updateCost
-                    self.neighbourCosts[self.myID][i] = self.minCosts[i]
-                    self.prepare_send()
+            if i == dest:
+                self.neighbourCosts[i] = newcost
+        self.myGUI.println("My news neighbours costs are :"+str(self.neighbourCosts))
+        self.myGUI.println("I going to compute my new min costs")
+        for i in range(self.sim.NUM_NODES):
+            if i!=self.myID:
+                self.myGUI.println("Compute min cost to go to "+ str(i)+" from "+ str(self.myID))
+                possibleCost = self.neighbourCosts[i] + self.routingTable[i][i]
+                self.myGUI.println(str(possibleCost))
+                for j in range(self.sim.NUM_NODES):
+                    if (j!=i and j!=self.myID):
+                        possibleCost1=self.neighbourCosts[j] +self.routingTable[j][i]
+                        self.myGUI.println("possible cost for "+ str(j)+ " is "+ str(possibleCost1))
+                        if possibleCost1 < possibleCost:
+                           possibleCost=possibleCost1
+                self.minCosts[i]=possibleCost
+                self.routingTable[self.myID][i]=self.minCosts[i]
+        self.prepare_send()
 
 
